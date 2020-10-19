f5f53bc500a94079bdfdd29ab33e708c3071c938
TinderSmith <coflery@gmail.com>
2020-10-19 14:19:01 +0800

    add edid length get from line argument

--- write-edid.sh
+++ write-edid.sh
@@ -13,14 +13,14 @@
 # and/or line breaks. For instance, this is the digital (!) EDID data
 # for a Samsung SyncMaster T220 monitor:
 #
-#   00 FF FF FF FF FF FF 00 4C 2D E5 03 32 32 57 54
-#   0E 12 01 03 80 2F 1E 78 2A EE 91 A3 54 4C 99 26
-#   0F 50 54 BF EF 80 B3 00 81 80 81 40 71 4F 01 01
-#   01 01 01 01 01 01 7C 2E 90 A0 60 1A 1E 40 30 20
-#   36 00 DA 28 11 00 00 1A 00 00 00 FD 00 38 4B 1E
-#   51 0E 00 0A 20 20 20 20 20 20 00 00 00 FC 00 53
-#   79 6E 63 4D 61 73 74 65 72 0A 20 20 00 00 00 FF
-#   00 48 39 58 51 34 30 31 30 34 38 0A 20 20 00 45
+#00 FF FF FF FF FF FF 00 4C 2D E5 03 32 32 57 54
+#0E 12 01 03 80 2F 1E 78 2A EE 91 A3 54 4C 99 26
+#0F 50 54 BF EF 80 B3 00 81 80 81 40 71 4F 01 01
+#01 01 01 01 01 01 7C 2E 90 A0 60 1A 1E 40 30 20
+#36 00 DA 28 11 00 00 1A 00 00 00 FD 00 38 4B 1E
+#51 0E 00 0A 20 20 20 20 20 20 00 00 00 FC 00 53
+#79 6E 63 4D 61 73 74 65 72 0A 20 20 00 00 00 FF
+#00 48 39 58 51 34 30 31 30 34 38 0A 20 20 00 45
 #
 # Take care, you can destroy some hardware by messing around with i2c-tools.
 # The scripts expects root permissions.
@@ -38,7 +38,7 @@ FILE=""
 
 if [ "$#" -lt "1" ]; then
   echo "Please specifiy a valid i2c bus as first parameter and the date as second one."
-  echo "Use: write-edid <bus> <data>"
+  echo "Use: write-edid <bus> <data> <edid-length>"
   exit 1
 fi
 
@@ -60,11 +60,17 @@ else
   fi
 fi
 
+# Make sure we get a edid length as command line argument.
+if [echo $3 | grep -q '[^0-9]'];then
+  echo "$3 : edid length must define as a number"
+  exit 3
+fi
+
 # Set loop separators to end of line and space
 IFSBAK=$IFS
 export IFS=$' \n\t\r'
 # some field
-edidLength=256
+edidLength="$3"
 count=0
 chipAddress="0x50"
 
@@ -88,7 +94,7 @@ do
   # increment counter
   count=$(($count+1))
   # sleep a moment
-  sleep 0.001s
+  sleep 0.1s
 done
 exec 0<&3
 
@@ -98,4 +104,3 @@ IFS=$IFSBAK
 echo "Writing done, here is the output of i2cdump -y $BUS 0x50:"
 i2cdump -y "$BUS" 0x50
 exit 0
-
